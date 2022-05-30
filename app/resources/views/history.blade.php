8af14f2313bf198eb986d2c8d3bd184dfdac3776
fatimazahra ait lasri <fatimazahraaitlasri96@gmail.com>
2022-05-31 00:40:12 +0100

    add responsive styling to history page

--- app/resources/views/history.blade.php
+++ app/resources/views/history.blade.php
@@ -2,21 +2,30 @@
 $creneauList = ['de 10h a 10h30', 'de 11h a 11h30', 'de 14h a 14h30', 'de 15h a 15h30', 'de 16h a 16h30'];
 $today = date('Y-m-d');
 @endphp
-
+@push('styles')
+    <link rel="stylesheet" href="css/history.css">
+@endpush
 @component('components.layout')
-    <div style="display:grid;grid-template-columns:repeat(3, 1fr);gap: 1rem">
-        @foreach ($list as $rdv)
-            <div style="background-color: grey">
-                <div style="display: flex; gap:1rem">
-                    @if ($today < $rdv['date'])
-                        <a href="{{ createLink('/rdv/update/' . $rdv['id']) }}">update</a>
-                    @endif
-                    <a href="{{ createLink('/rdv/delete/' . $rdv['id']) }}">delete</a>
+    <div class="history">
+        <a class="create" href="{{ createLink('/create') }}">nouveau rendez vous</a>
+        <div class="list">
+
+            @foreach ($list as $rdv)
+                <div class="card">
+                    <div class="controls">
+                        @if ($today < $rdv['date'])
+                            <a href="{{ createLink('/rdv/update/' . $rdv['id']) }}">update</a>
+                        @endif
+                        <a href="{{ createLink('/rdv/delete/' . $rdv['id']) }}">delete</a>
+                    </div>
+                    <div class="section"><span class="label">sujet:</span> <span
+                            class="value">{{ $rdv['sujet'] }}</span></div>
+                    <div class="section"><span class="label">creneau:</span> <span
+                            class="value">{{ $creneauList[$rdv['creneau']] }}</span></div>
+                    <div class="section"><span class="label">date:</span> <span
+                            class="value">{{ $rdv['date'] }}</span></div>
                 </div>
-                <p>sujet : {{ $rdv['sujet'] }}</p>
-                <p>creneau : {{ $creneauList[$rdv['creneau']] }}</p>
-                <p>date : {{ $rdv['date'] }}</p>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endcomponent
